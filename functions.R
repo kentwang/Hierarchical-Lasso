cae78de40085fa35c55dca1a3e2a5a5ffb9e10ff
ketongwang <kentwang16@gmail.com>
2015-04-23 16:41:47 -0500

    - added cross-validation and prediction for grplasso

--- functions.R
+++ functions.R
@@ -34,3 +34,48 @@ predEvaluation = function(data, flma, method) {
   X = model.matrix(fmla,data)
   Y = as.matrix(data[,as.character(fmla[[2]])])
 }
+
+#== cross validation for grplasso
+# Z_dummy[id.train, ]; y = y[id.train]
+cv.grplasso.my = function(x, y, index, lambda, model, ...) {
+  n = length(y)
+  p = dim(x)[2]
+  nlambda = length(lambda)
+  
+  # define the output object
+  result = list()
+  result$lambda = lambda
+  result$model = model
+  result$index = index
+    
+  foldid = cvfolds(n, seed = 20150423)
+  CVError = rep(0, nlambda)
+  
+  K = sort(unique(foldid))
+  for(k in K) {
+    # cat("iter", k, "\n")
+    test = which(foldid == k)
+    train = which(foldid != k)
+    fit = grplasso(x[train, ], y = y[train], index = index, model = model, 
+                   lambda = lambda, control = grpl.control(trace = 0), ...) 
+    pred.grplasso = predict(fit, newdata = x[test, ], type = "response")
+    CVError = CVError + apply(pred.grplasso, 2, FUN = function(v) mean((v - y[test])^2))
+  }
+  CVError = CVError/length(K)
+  
+  result$CVError = CVError
+  result$lambda.min = lambda[which.min(CVError)]
+  result$fit = grplasso(x, y = y, index = index,model = model, 
+                        lambda = lambda, control = grpl.control(trace = 0)) 
+  
+  return(result)
+}
+
+##== Prediction for cv.grplasso.my
+predict.cv.grplasso.my = function(fit.cv.grplasso, newdata, s, type = "response") {
+  id = which(fit.cv.grplasso$lambda == s)
+  prediction = predict(fit.cv.grplasso$fit, newdata = newdata, type = type)[, id]
+  return(prediction)
+}
+
+
